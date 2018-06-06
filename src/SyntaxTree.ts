5209aae3713babe1fda2d3cd3e31b0478a26ca7f
ecoulson <ivansonofcoul@gmail.com>
2018-06-05 22:49:14 -0700

    added beginnings of test for syntax tree

--- src/SyntaxTree.ts
+++ src/SyntaxTree.ts
@@ -1,11 +1,9 @@
 import { Token } from "./Token";
 
-namespace ExpressionEvaluator {
-	export class SyntaxTree {
-		private tokens: Array<Token>;
-	
-		constructor(tokens: Array<Token>) {
-			this.tokens = tokens;
-		}
+export class SyntaxTree {
+	private tokens: Array<Token>;
+
+	constructor(tokens: Array<Token>) {
+		this.tokens = tokens;
 	}
 }
